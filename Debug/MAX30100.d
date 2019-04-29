5f5019db37b2f0f95a02f402da8b69f36c3196bc
Eisen <1725979950@qq.com>
2019-04-29 22:53:46 +0800

    v2

---
+++ Debug/MAX30100.d
@@ -0,0 +1,35 @@
+# FIXED
+
+MAX30100.obj: ../MAX30100.c
+MAX30100.obj: ../MAX30100.h
+MAX30100.obj: C:/ti/ccsv8/tools/compiler/ti-cgt-msp430_18.1.1.LTS/include/stdint.h
+MAX30100.obj: C:/ti/ccsv8/tools/compiler/ti-cgt-msp430_18.1.1.LTS/include/sys/stdint.h
+MAX30100.obj: C:/ti/ccsv8/tools/compiler/ti-cgt-msp430_18.1.1.LTS/include/sys/cdefs.h
+MAX30100.obj: C:/ti/ccsv8/tools/compiler/ti-cgt-msp430_18.1.1.LTS/include/sys/_types.h
+MAX30100.obj: C:/ti/ccsv8/tools/compiler/ti-cgt-msp430_18.1.1.LTS/include/machine/_types.h
+MAX30100.obj: C:/ti/ccsv8/tools/compiler/ti-cgt-msp430_18.1.1.LTS/include/machine/_stdint.h
+MAX30100.obj: C:/ti/ccsv8/tools/compiler/ti-cgt-msp430_18.1.1.LTS/include/sys/_stdint.h
+MAX30100.obj: ../MAX30100_Registers.h
+MAX30100.obj: ../IIC.h
+MAX30100.obj: C:/ti/ccsv8/ccs_base/msp430/include/msp430.h
+MAX30100.obj: C:/ti/ccsv8/ccs_base/msp430/include/msp430fr6989.h
+MAX30100.obj: C:/ti/ccsv8/ccs_base/msp430/include/in430.h
+MAX30100.obj: C:/ti/ccsv8/tools/compiler/ti-cgt-msp430_18.1.1.LTS/include/intrinsics.h
+MAX30100.obj: C:/ti/ccsv8/tools/compiler/ti-cgt-msp430_18.1.1.LTS/include/intrinsics_legacy_undefs.h
+
+../MAX30100.c: 
+../MAX30100.h: 
+C:/ti/ccsv8/tools/compiler/ti-cgt-msp430_18.1.1.LTS/include/stdint.h: 
+C:/ti/ccsv8/tools/compiler/ti-cgt-msp430_18.1.1.LTS/include/sys/stdint.h: 
+C:/ti/ccsv8/tools/compiler/ti-cgt-msp430_18.1.1.LTS/include/sys/cdefs.h: 
+C:/ti/ccsv8/tools/compiler/ti-cgt-msp430_18.1.1.LTS/include/sys/_types.h: 
+C:/ti/ccsv8/tools/compiler/ti-cgt-msp430_18.1.1.LTS/include/machine/_types.h: 
+C:/ti/ccsv8/tools/compiler/ti-cgt-msp430_18.1.1.LTS/include/machine/_stdint.h: 
+C:/ti/ccsv8/tools/compiler/ti-cgt-msp430_18.1.1.LTS/include/sys/_stdint.h: 
+../MAX30100_Registers.h: 
+../IIC.h: 
+C:/ti/ccsv8/ccs_base/msp430/include/msp430.h: 
+C:/ti/ccsv8/ccs_base/msp430/include/msp430fr6989.h: 
+C:/ti/ccsv8/ccs_base/msp430/include/in430.h: 
+C:/ti/ccsv8/tools/compiler/ti-cgt-msp430_18.1.1.LTS/include/intrinsics.h: 
+C:/ti/ccsv8/tools/compiler/ti-cgt-msp430_18.1.1.LTS/include/intrinsics_legacy_undefs.h: 
